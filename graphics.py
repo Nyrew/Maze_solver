7ec3f280c1b5eb4feeebd0cae00b8f725b5d3d38
Nyrew <lukas.vokel@gmail.com>
2024-10-30 09:18:02 +0100

    Maze class

--- graphics.py
+++ graphics.py
@@ -20,27 +20,29 @@ class Window:
             self.redraw()
         print("window closed...")
     
-    def draw_line(self, line, fill_color):
+    def draw_line(self, line, fill_color="black"):
         line.draw(self.__canvas, fill_color)
-        
+
     def close(self):
         self.__running = False
 
 
-class Point():
-    def __init__(self, x, y) -> None:
+class Point:
+    def __init__(self, x, y):
         self.x = x
         self.y = y
 
-class Line():
-    def __init__(self, point1, point2) -> None:
-        self.point1 = point1
-        self.point2 = point2
-    
-    def draw(self, canvas, fill_color):
-        canvas.create_line(
-            self.point1.x, self.point1.y, 
-            self.point2.x, self.point2.y, 
-            fill=fill_color, width=2
-        )
 
+class Line:
+    def __init__(
+        self,
+        p1,
+        p2,
+    ):
+        self.p1 = p1
+        self.p2 = p2
+
+    def draw(self, canvas, fill_color="black"):
+        canvas.create_line(
+            self.p1.x, self.p1.y, self.p2.x, self.p2.y, fill=fill_color, width=2
+        )
